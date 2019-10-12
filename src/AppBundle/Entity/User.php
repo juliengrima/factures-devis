eb00985cdc421427d350028ebdea293a4af4a805
Julien <gogrinformatique@gmail.com>
2019-10-12 23:53:14 +0200

    fos user add

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * User
+ */
+class User
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
